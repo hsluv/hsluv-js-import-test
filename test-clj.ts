503da009015802ada0a2f64237d871c3f7ee6354
Alexei Boronine <alexei@boronine.com>
2023-07-23 23:17:26 +0000

    wip

---
+++ test-clj.ts
@@ -0,0 +1,8 @@
+const {Hsluv} = require("hsluv");
+
+var conv = new Hsluv();
+conv.hsluv_h = 10;
+conv.hsluv_s = 75;
+conv.hsluv_l = 65;
+conv.hsluvToHex();
+console.log(conv.hex); // Will print "#ec7d82"
---
+++ test-esm.ts
@@ -0,0 +1,1 @@
+import * as hsluv from 'hsluv';
